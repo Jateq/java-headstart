057c808924e1cbf57b6796a101d99e32d868c088
jateq <temirlan.eraly1@gmail.com>
2023-11-01 00:32:23 +0600

    scan

---
+++ start/src/main/UserScan.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class UserScan {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("What is your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello "+name);
+        
+        
+        
+    }
+
+}
